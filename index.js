c69fc3659b1184e8e9bb68ee250cdd4e4e7d870b
William Sinn <w.h.sinn@gmail.com>
2019-05-11 17:00:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let counter = 0;
+  let namesArr = [];
+  for (let i=0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    namesArr.push(message)
+    counter++;
+  }
+  return namesArr;
+  }
+
+function countdown(num) {
+  let count = num;
+  while (count > 0) {
+    console.log(count);
+    count--;
+  }
+  console.log(count);
+}
